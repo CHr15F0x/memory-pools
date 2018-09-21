d5045c41349d8c2eead88bb0fa819928f15ea452
Krzysiek Lis <klis33@gmail.com>
2018-09-21 11:58:31 +0200

    Compact pool - initial version

--- kl-compact-pool.h
+++ kl-compact-pool.h
@@ -1,39 +1,98 @@
 #ifndef KL_COMPACT_POOL_H_
 #define KL_COMPACT_POOL_H_
 
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
 #include <cstddef>
+#include <cstdlib>
+#include <type_traits>
+
+#define KL_COMPACT_POOL_RUNTIME_CHECKS 1
+
+#if KL_COMPACT_POOL_RUNTIME_CHECKS
+#define KL_COMPACT_POOL_CHECK_POINTER(OFFSET, CAPACITY, PTR)\
+    assert(((OFFSET) >= 0u) || ("Pointer does not belong to pool." == 0));\
+    assert(((OFFSET) < static_cast<ptrdiff_t>(CAPACITY)) || ("Pointer does not belong to pool." == 0));\
+    assert((reinterpret_cast<uintptr_t>(PTR) % alignof(aligned_storage_t) == 0) || ("Pointer is not aligned properly" == 0))
+#define KL_COMPACT_POOL_CHECK_CAPACITY(CAPACITY)\
+    assert((capacity > 0u) || (KL_MIN_CAPACITY_MSG == 0));\
+    assert((capacity <= private_internal::MAX_SUPPORTED_CAPACITY) || (KL_MAX_CAPACITY_MSG == 0));
+#else
+#define KL_COMPACT_POOL_CHECK_POINTER(OFFSET, CAPACITY, PTR)
+#define KL_COMPACT_POOL_CHECK_CAPACITY(CAPACITY)
+#endif // KL_COMPACT_POOL_RUNTIME_CHECKS
 
 namespace kl
 {
-namespace internal
+// Internal constants and functions, not a part of the public API
+namespace private_internal
 {
-#if UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFUL
+// 64 bit arch
+#if UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFul
 typedef uint_fast64_t cpu_word_t;
-constexpr size_t BITS = 64;
-#elif UINTPTR_MAX == 0xFFFFFFFFUL
+constexpr size_t BITS = 64u;
+constexpr size_t SHIFT = 6u;
+constexpr cpu_word_t MASK = 0x3Fu;
+// 32 bit arch
+#elif UINTPTR_MAX == 0xFFFFFFFFul
 typedef uint_fast32_t cpu_word_t;
-constexpr size_t BITS = 32;
+constexpr size_t BITS = 32u;
+constexpr size_t SHIFT = 5u;
+constexpr cpu_word_t MASK = 0x1Fu;
 #else
 #error "Unsupported platform!"
 #endif
 
+// Width of this constant must be the same as word to achieve proper shifts
 constexpr cpu_word_t ONE = static_cast<cpu_word_t>(1);
-constexpr size_t FULL = UINTPTR_MAX;
+constexpr cpu_word_t FULL = UINTPTR_MAX;
+constexpr size_t MAX_SUPPORTED_DEPTH = 3u;
+constexpr size_t MAX_SUPPORTED_CAPACITY = ONE << (SHIFT * (MAX_SUPPORTED_DEPTH + 1u));
+#define KL_MIN_CAPACITY_MSG "Pool has to be able to hold at least one object."
+#define KL_MAX_CAPACITY_MSG "Maximum capacity is elements (64|32)^4 depending on architecture."
+
+template<class T>
+inline constexpr T Pow(const T base, cpu_word_t const exponent)
+{
+    return (exponent == 0) ? 1 : (base * Pow(base, exponent - 1));
+}
+
+template<class T>
+inline constexpr size_t GetLevel(const T capacity, const size_t level = MAX_SUPPORTED_DEPTH)
+{
+    return level == 0 ? 0 : (capacity - 1) & (private_internal::MASK << (private_internal::SHIFT * (level))) ? level : GetLevel(capacity, level - 1u);
+}
+
+template<class T>
+inline constexpr T MaxTreeSize(const T level)
+{
+    return (level == 0) ? 1 : Pow(BITS, level) + MaxTreeSize(level - 1u);
+}
+
+template<class T>
+inline constexpr T OptimalAllocatedSize2(const T capacity, const size_t level)
+{
+    return (level == 0) ? 1 : (capacity - 1) / BITS + 1 + MaxTreeSize(level - 1u);
+}
+
+constexpr size_t BITS2 = Pow(BITS, 2);
+constexpr size_t BITS3 = Pow(BITS, 3);
+constexpr size_t BITS4 = Pow(BITS, 4);
 
 inline cpu_word_t LeastBitClear(cpu_word_t word)
 {
-    cpu_word_t out;
+    cpu_word_t out = 0u;
     asm("bsf %1, %0"
-        : "=rm" (out)
-        : "rm" (~word));
+        : "=r" (out)
+        : "r" (~word));
     return out;
 }
 
 template <size_t Level>
 size_t OffsetAndMark(cpu_word_t *allocated)
-{ 
+{
+    (void)allocated;
     assert("Only specialised instances of this function make sense." == 0);
     return 0;
 }
@@ -61,9 +120,309 @@ size_t OffsetAndMark<1>(cpu_word_t *allocated)
         allocated[0] |= (ONE << offset_on_level0);
     }
     
-    assert(offset < (BITS * BITS));
+    assert(offset < BITS2);
     return offset;
 }
-} // namespace internal
+
+template<>
+size_t OffsetAndMark<2>(cpu_word_t *allocated)
+{
+    size_t offset_on_level0 = LeastBitClear(allocated[0]);
+    size_t offset_on_level1 = LeastBitClear(allocated[1 + offset_on_level0]);
+    size_t offset_on_level2 = LeastBitClear(allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1]);
+    size_t offset = offset_on_level0 * BITS2 + offset_on_level1 * BITS + offset_on_level2;
+
+    allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1] |= (ONE << offset_on_level2);
+
+    if (allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1] == FULL)
+    {
+        allocated[1 + offset_on_level0] |= (ONE << offset_on_level1);
+
+        if (allocated[1 + offset_on_level0] == FULL)
+        {
+            allocated[0] |= (ONE << offset_on_level0);
+        }
+    }
+
+    assert(offset < BITS3);
+    return offset;
+}
+
+template<>
+size_t OffsetAndMark<3>(cpu_word_t *allocated)
+{
+    size_t offset_on_level0 = LeastBitClear(allocated[0]);
+    size_t offset_on_level1 = LeastBitClear(allocated[1 + offset_on_level0]);
+    size_t offset_on_level2 = LeastBitClear(allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1]);
+    size_t offset_on_level3 = LeastBitClear(allocated[1 + BITS + BITS2 + BITS2 * offset_on_level0 + BITS * offset_on_level1 + offset_on_level2]);
+    size_t offset = offset_on_level0 * BITS3 + offset_on_level1 * BITS2 + offset_on_level2 * BITS + offset_on_level3;
+
+    allocated[1 + BITS + BITS2 + BITS2 * offset_on_level0 + BITS * offset_on_level1 + offset_on_level2] |= (ONE << offset_on_level3);
+
+    if (allocated[1 + BITS + BITS2 + BITS2 * offset_on_level0 + BITS * offset_on_level1 + offset_on_level2] == FULL)
+    {
+        allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1] |= (ONE << offset_on_level2);
+
+        if (allocated[1 + BITS + offset_on_level0 * BITS + offset_on_level1] == FULL)
+        {
+            allocated[1 + offset_on_level0] |= (ONE << offset_on_level1);
+
+            if (allocated[1 + offset_on_level0] == FULL)
+            {
+                allocated[0] |= (ONE << offset_on_level0);
+            }
+        }
+    }
+
+    assert(offset < BITS4);
+    return offset;
+}
+
+template <size_t Level>
+void Unmark(cpu_word_t *allocated, size_t offset)
+{
+    (void)allocated;
+    (void)offset;
+    assert("Only specialised instances of this function make sense." == 0);
+}
+
+template <>
+void Unmark<0>(cpu_word_t *allocated, size_t offset)
+{
+    allocated[0] &= ~(ONE << offset);
+}
+
+// TODO check if shift & bithacks for division and modulo are faster
+
+template <>
+void Unmark<1>(cpu_word_t *allocated, size_t offset)
+{
+    allocated[offset / BITS + 1] &= ~(ONE << (offset % BITS));
+
+    if (allocated[offset / BITS + 1] < FULL)
+    {
+        allocated[0] &= ~(ONE << (offset / BITS));
+    }
+}
+
+template <>
+void Unmark<2>(cpu_word_t *allocated, size_t offset)
+{
+    allocated[offset / BITS + BITS + 1] &= ~(ONE << (offset % BITS));
+
+    if (allocated[offset / BITS + BITS + 1] < FULL)
+    {
+        allocated[offset / BITS2 + 1] &= ~(ONE << ((offset / BITS) % BITS));
+
+        if (allocated[offset / BITS2 + 1] < FULL)
+        {
+            allocated[0] &= ~(ONE << (offset / BITS2));
+        }
+    }
+}
+
+template <>
+void Unmark<3>(cpu_word_t *allocated, size_t offset)
+{
+    allocated[offset / BITS + BITS2 + BITS + 1] &= ~(ONE << (offset % BITS));
+
+    if (allocated[offset / BITS + BITS2 + BITS + 1] < FULL)
+    {
+        allocated[offset / BITS2 + BITS + 1] &= ~(ONE << ((offset / BITS) % BITS));
+
+        if (allocated[offset / BITS2 + BITS + 1] < FULL)
+        {
+            allocated[offset / BITS3 + 1] &= ~(ONE << ((offset / BITS2) % BITS));
+
+            if (allocated[offset / BITS3 + 1] < FULL)
+            {
+                allocated[0] &= ~(ONE << (offset / BITS3));
+            }
+        }
+    }
+}
+} // namespace private_internal
+
+// Capacity set at compile time
+template<typename T, size_t capacity>
+class CompactPool
+{
+private:
+
+    static_assert(capacity > 0, KL_MIN_CAPACITY_MSG);
+    static_assert(capacity <= (private_internal::MAX_SUPPORTED_CAPACITY), KL_MAX_CAPACITY_MSG);
+
+    static constexpr size_t level = private_internal::GetLevel(capacity);
+    typedef typename std::aligned_storage<sizeof(T), alignof(T)>::type aligned_storage_t;
+    uint_fast32_t size_;
+    uint_fast32_t max_utilisation_;
+    aligned_storage_t buffer[capacity];
+    private_internal::cpu_word_t allocated[private_internal::OptimalAllocatedSize2(capacity, level)];
+
+public:
+
+    CompactPool() :
+        size_(0u),
+        max_utilisation_(0u),
+        buffer(),
+        allocated()
+    {}
+
+    ~CompactPool()
+    {}
+
+    T *Alloc()
+    {
+        if (size_ == capacity)
+        {
+            // Full
+            return nullptr;
+        }
+
+        ++size_;
+        max_utilisation_ = std::max(size_, max_utilisation_);
+        size_t offset = private_internal::OffsetAndMark<level>(allocated);
+        return reinterpret_cast<T *>(&buffer[offset]);
+    }
+
+    void Free(T *p)
+    {
+        ptrdiff_t offset = reinterpret_cast<aligned_storage_t *>(p) - buffer;
+        KL_COMPACT_POOL_CHECK_POINTER(offset, capacity, p);
+        private_internal::Unmark<level>(allocated, offset);
+        --size_;
+    }
+
+    size_t Size() const
+    {
+        return size_;
+    }
+
+    size_t MaxUtilisation() const
+    {
+        return max_utilisation_;
+    }
+
+    constexpr size_t Capacity()
+    {
+        return capacity;
+    }
+
+    constexpr size_t MaxCapacity()
+    {
+        return private_internal::MAX_SUPPORTED_CAPACITY;
+    }
+}; // class CompactPool
+
+// Capacity set in runtime
+template<typename T>
+class CompactPool2
+{
+private:
+
+    typedef typename std::aligned_storage<sizeof(T), alignof(T)>::type aligned_storage_t;
+    typedef size_t (* OffsetAndMarkFn)(private_internal::cpu_word_t *allocated);
+    typedef void (* UnmarkFn)(private_internal::cpu_word_t *allocated, size_t offset);
+    uint_fast32_t capacity_;
+    uint_fast32_t size_;
+    uint_fast32_t max_utilisation_;
+    uint_fast32_t depth_;
+    aligned_storage_t *buffer_;
+    private_internal::cpu_word_t *allocated_;
+    OffsetAndMarkFn offset_and_mark_;
+    UnmarkFn unmark_;
+
+    OffsetAndMarkFn ChooseOffsetAndMarkFn()
+    {
+        switch(depth_)
+        {
+            case 0u: return private_internal::OffsetAndMark<0>;
+            case 1u: return private_internal::OffsetAndMark<1>;
+            case 2u: return private_internal::OffsetAndMark<2>;
+            case 3u: return private_internal::OffsetAndMark<3>;
+            default:
+                assert("Invalid depth." == 0);
+                return nullptr;
+        }
+    }
+
+    UnmarkFn ChooseUnmarkFn()
+    {
+        switch(depth_)
+        {
+            case 0u: return private_internal::Unmark<0>;
+            case 1u: return private_internal::Unmark<1>;
+            case 2u: return private_internal::Unmark<2>;
+            case 3u: return private_internal::Unmark<3>;
+            default:
+                assert("Invalid depth." == 0);
+                return nullptr;
+        }
+    }
+
+public:
+
+    CompactPool2(size_t capacity) :
+        capacity_(capacity),
+        size_(0u),
+        max_utilisation_(0u),
+        depth_(private_internal::GetLevel(capacity)),
+        buffer_(reinterpret_cast<aligned_storage_t *>(std::malloc(capacity_ * sizeof(aligned_storage_t)))),
+        allocated_(reinterpret_cast<private_internal::cpu_word_t *>(std::calloc(private_internal::OptimalAllocatedSize2(capacity_, depth_), sizeof(private_internal::cpu_word_t)))),
+        offset_and_mark_(ChooseOffsetAndMarkFn()),
+        unmark_(ChooseUnmarkFn())
+    {
+        KL_COMPACT_POOL_CHECK_CAPACITY(capacity_);
+    }
+
+    ~CompactPool2()
+    {
+        std::free(buffer_);
+        std::free(allocated_);
+    }
+
+    T *Alloc()
+    {
+        if (size_ == capacity_)
+        {
+            // Full
+            return nullptr;
+        }
+
+        ++size_;
+        max_utilisation_ = std::max(size_, max_utilisation_);
+        size_t offset = offset_and_mark_(allocated_);
+        return reinterpret_cast<T *>(&buffer_[offset]);
+    }
+
+    void Free(T *p)
+    {
+        ptrdiff_t offset = reinterpret_cast<aligned_storage_t *>(p) - buffer_;
+        KL_COMPACT_POOL_CHECK_POINTER(offset, capacity_, p);
+        unmark_(allocated_, offset);
+        --size_;
+    }
+
+    size_t Size() const
+    {
+        return size_;
+    }
+
+    size_t MaxUtilisation() const
+    {
+        return max_utilisation_;
+    }
+
+    size_t Capacity() const
+    {
+        return capacity_;
+    }
+
+    constexpr size_t MaxCapacity()
+    {
+        return private_internal::MAX_SUPPORTED_CAPACITY;
+    }
+}; // class CompactPool2
+
 } // namespace kl
 #endif // KL_COMPACT_POOL_H_
--- tests/compact-pool-test.cpp
+++ tests/compact-pool-test.cpp
@@ -2,69 +2,240 @@
 #include <gtest.h>
 
 using namespace kl;
+using namespace kl::private_internal;
 
-// TODO add 32 bit cases
+// TODO build and run on 32bits
+// TODO build and run on 32bits
+// TODO build and run on 32bits
 
-TEST(CompactPool, OffsetAndMark0)
+// Expectations for bits set
+static const cpu_word_t kNoBitsSet = static_cast<cpu_word_t>(0u);
+static const cpu_word_t k1stBitSet = static_cast<cpu_word_t>(1u);
+static const cpu_word_t kAllBut1BitsSet = (~kNoBitsSet) >> 1u;
+
+TEST(private_internal, LeastBitClear)
 {
-    // 0 level tree holds up to 64^1 elements
-    internal::cpu_word_t allocated[1] = { 0u };
-    
-    // Insert 1st element into the tree
-    EXPECT_EQ(0u, internal::OffsetAndMark<0u>(allocated));
-    EXPECT_EQ(0x01U, allocated[0]);
-    
-    // Insert 64th element (last)
-    allocated[0] = 0x7Fu;
-    EXPECT_EQ(7u, internal::OffsetAndMark<0u>(allocated));
-    EXPECT_EQ(0xFFu, allocated[0]);
+    EXPECT_EQ(0u, LeastBitClear(kNoBitsSet));
+    EXPECT_EQ(1u, LeastBitClear(k1stBitSet));
+    EXPECT_EQ(BITS - 1, LeastBitClear(kAllBut1BitsSet));
 }
 
-TEST(CompactPool, OffsetAndMark1)
+struct GetLevelTestData
 {
-    // 1 level tree holds up to 64^2 elements
-    internal::cpu_word_t allocated[1 + 64] = { 0u };
+    size_t expected_depth;
+    size_t input_capacity;
+};
 
-    // Insert 1st element into the tree
-    EXPECT_EQ(0u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x00u, allocated[0]);
-    EXPECT_EQ(0x01u, allocated[1]);
+struct GetLevelTest: public ::testing::TestWithParam<GetLevelTestData>
+{
+    GetLevelTestData test_data;
+};
 
-    // Insert 8th
-    allocated[1] = 0x7Fu;
-    EXPECT_EQ(7u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x00u, allocated[0]);
-    EXPECT_EQ(0xFFu, allocated[1]);
-    
-    // Insert 9th
-    EXPECT_EQ(8u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x00u, allocated[0]);
-    EXPECT_EQ(0x1FFu, allocated[1]);
-    
-    // Insert 63rd
-    allocated[1] = 0x3FFFFFFFFFFFFFFFu;
-    EXPECT_EQ(62u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x00u, allocated[0]);
-    EXPECT_EQ(0x7FFFFFFFFFFFFFFFu, allocated[1]);
-    
-    // Insert 64th
-    allocated[1] = 0x7FFFFFFFFFFFFFFFu;
-    EXPECT_EQ(63u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x01u, allocated[0]);
-    EXPECT_EQ(0xFFFFFFFFFFFFFFFFu, allocated[1]);
+TEST_P(GetLevelTest, GetLevel)
+{
+    EXPECT_EQ(GetParam().expected_depth, GetLevel(GetParam().input_capacity));
+}
 
-    // Insert 4095th
-    memset(allocated, 0xFF, sizeof(allocated));
-    allocated[0] = 0x7FFFFFFFFFFFFFFFu;
-    allocated[64] = 0x3FFFFFFFFFFFFFFFu;
-    EXPECT_EQ(4094u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0x7FFFFFFFFFFFFFFFu, allocated[0]);
-    EXPECT_EQ(0x7FFFFFFFFFFFFFFFu, allocated[64]);
+GetLevelTestData kGetLevelTestCases[] =
+{
+    {0u, 1u},
+    {0u, BITS - 1u},
+    {0u, BITS},
+    {1u, BITS + 1u},
+    {1u, BITS2 - 1u},
+    {1u, BITS2},
+    {2u, BITS2 + 1u},
+    {2u, BITS3 - 1u},
+    {2u, BITS3},
+    {3u, BITS3 + 1u},
+    {3u, BITS4 - 1u},
+    {3u, BITS4}
+};
 
-    // Insert 4096th (last)
-    EXPECT_EQ(4095u, internal::OffsetAndMark<1u>(allocated));
-    EXPECT_EQ(0xFFFFFFFFFFFFFFFFu, allocated[0]);
-    EXPECT_EQ(0xFFFFFFFFFFFFFFFFu, allocated[64]);
+INSTANTIATE_TEST_CASE_P(private_internal, GetLevelTest, ::testing::ValuesIn(kGetLevelTestCases));
+
+struct GetTreeSizeTestData
+{
+    size_t expected_tree_size;
+    size_t input_capacity;
+};
+
+struct GetTreeSizeTest: public ::testing::TestWithParam<GetTreeSizeTestData>
+{
+    GetTreeSizeTestData test_data;
+};
+
+TEST_P(GetTreeSizeTest, GetTreeSize)
+{
+    EXPECT_EQ(GetParam().expected_tree_size, OptimalAllocatedSize2(GetParam().input_capacity, GetLevel(GetParam().input_capacity)));
+}
+
+GetTreeSizeTestData kGetTreeSizeTestCases[] =
+{
+    {1u, 1u},
+    {1u, BITS - 1u},
+    {1u, BITS},
+    {3u, BITS + 1u},
+    {3u, BITS * 2},
+    {4u, BITS * 2 + 1u},
+    {BITS, BITS2 - BITS},
+    {BITS + 1u, BITS2 - 1u},
+    {BITS + 1u, BITS2},
+    {BITS2 + BITS + 1u, BITS3},
+    {BITS2 + 2u + BITS2 + BITS + 1u, BITS3 + BITS + 9u},
+    {BITS2 + 1u + BITS2 + BITS + 1u, BITS3 + 1u},
+    {BITS3 + BITS2 + BITS + 1u, BITS4 - 1u},
+    {BITS3 + BITS2 + BITS + 1u, BITS4}
+};
+
+INSTANTIATE_TEST_CASE_P(private_internal, GetTreeSizeTest, ::testing::ValuesIn(kGetTreeSizeTestCases));
+
+template <size_t Depth>
+struct TreeDepth
+{
+    static constexpr size_t depth = Depth;
+    static constexpr size_t max_capacity = Pow(BITS, depth);
+    static constexpr size_t masks_size = MaxTreeSize(depth);
+};
+
+template<typename TreeDepthType>
+struct TreeDepthTest : public ::testing::Test
+{};
+
+typedef ::testing::Types<
+    TreeDepth<0u>,
+    TreeDepth<1u>,
+    TreeDepth<2u>,
+    TreeDepth<3u>
+> TreeDepthTypes;
+
+TYPED_TEST_CASE(TreeDepthTest, TreeDepthTypes);
+
+TYPED_TEST(TreeDepthTest, private_internal_OffsetAndMark_AndThen_Unmark)
+{
+    cpu_word_t mask_tree[TypeParam::masks_size];
+    memset(mask_tree, 0u, sizeof(mask_tree));
+
+    for (size_t i = 0; i < TypeParam::max_capacity; ++i)
+    {
+        ASSERT_EQ(i, OffsetAndMark<TypeParam::depth>(mask_tree));
+        Unmark<TypeParam::depth>(mask_tree, i);
+        ASSERT_EQ(i, OffsetAndMark<TypeParam::depth>(mask_tree));
+    }
+}
+
+struct A
+{
+    cpu_word_t data;
+};
+
+template <typename PoolType>
+static void PoolTestFn(PoolType &pool)
+{
+    const size_t CAPACITY = pool.Capacity();
+
+    EXPECT_EQ(0u, pool.Size());
+    EXPECT_EQ(0u, pool.MaxUtilisation());
+
+    std::vector<A *> ptrs(CAPACITY);
+    std::vector<size_t> order_of_frees(CAPACITY);
+    // Fill with 0, 1, 2, ...
+    std::iota(order_of_frees.begin(), order_of_frees.end(), 0u);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // Make it random
+    std::shuffle(order_of_frees.begin(), order_of_frees.end(), gen);
+
+    for (size_t i = 0; i < order_of_frees.size(); ++i)
+    {
+        ASSERT_EQ(i, pool.Size());
+        ptrs[i] = pool.Alloc();
+        // Some fake data
+        ptrs[i]->data = i;
+        // Size increased
+        ASSERT_EQ(i + 1, pool.Size());
+    }
+
+    // Full
+    EXPECT_EQ(nullptr, pool.Alloc());
+
+    for (int i = CAPACITY - 1; i >= 0; --i)
+    {
+        ASSERT_EQ(static_cast<size_t>(i + 1), pool.Size());
+        ASSERT_EQ(static_cast<cpu_word_t>(order_of_frees[i]), ptrs[order_of_frees[i]]->data);
+        pool.Free(ptrs[i]);
+        // Free does not wipe the data, so check if previous free has not corrupted this entry
+        ASSERT_EQ(static_cast<cpu_word_t>(order_of_frees[i]), ptrs[order_of_frees[i]]->data);
+        // Now we can clear it
+        ptrs[order_of_frees[i]]->data = 0u;
+        // Size decreased
+        ASSERT_EQ(static_cast<size_t>(i), pool.Size());
+    }
+
+    // Re-shuffle
+    std::shuffle(order_of_frees.begin(), order_of_frees.end(), gen);
+
+    for (size_t i = 0; i < order_of_frees.size(); ++i)
+    {
+        ASSERT_EQ(i, pool.Size());
+        ptrs[i] = pool.Alloc();
+        // Other fake data
+        ptrs[i]->data = i * i;
+        // Size increased
+        ASSERT_EQ(i + 1, pool.Size());
+    }
+
+    for (int i = CAPACITY - 1; i >= 0; --i)
+    {
+        ASSERT_EQ(static_cast<size_t>(i + 1), pool.Size());
+        ASSERT_EQ(static_cast<cpu_word_t>(order_of_frees[i]) * static_cast<cpu_word_t>(order_of_frees[i]), ptrs[order_of_frees[i]]->data);
+        pool.Free(ptrs[i]);
+        // Free does not wipe the data, so check if previous free has not corrupted this entry
+        ASSERT_EQ(static_cast<cpu_word_t>(order_of_frees[i]) * static_cast<cpu_word_t>(order_of_frees[i]), ptrs[order_of_frees[i]]->data);
+        // Now we can clear it
+        ptrs[order_of_frees[i]]->data = 0u;
+        // Size decreased
+        ASSERT_EQ(static_cast<size_t>(i), pool.Size());
+    }
+}
+
+template<typename CompactPoolType>
+struct CompactPoolTest : public ::testing::Test
+{};
+
+typedef ::testing::Types<
+    CompactPool<A, 1u>,
+    CompactPool<A, 13u>,
+    CompactPool<A, BITS>,
+    CompactPool<A, 811u>,
+    CompactPool<A, BITS2>,
+    CompactPool<A, 11113u>,
+    CompactPool<A, BITS3>,
+    CompactPool<A, 262217u>,
+    CompactPool<A, BITS4>
+> CompactPoolTypes;
+
+TYPED_TEST_CASE(CompactPoolTest, CompactPoolTypes);
+
+TYPED_TEST(CompactPoolTest, AllocAndRandomFree_Twice_NoMemoryCorruption)
+{
+    // This kind of pool has to be static, using it on the stack could easily lead to overflow
+    static TypeParam pool;
+    PoolTestFn(pool);
+}
+
+TEST(CompactPool2Test, AllocAndRandomFree_Twice_NoMemoryCorruption)
+{
+    const size_t CAPCITIES[] =
+    {
+        1u, 13u, BITS, 811u, BITS2, 11113u, BITS3, 262217u, BITS4
+    };
+
+    for (size_t k = 0; k < sizeof(CAPCITIES) / sizeof(CAPCITIES[0]); ++k)
+    {
+        CompactPool2<A> pool(CAPCITIES[k]);
+        PoolTestFn(pool);
+    }
 }
 
 int main(int argc, char **argv)
